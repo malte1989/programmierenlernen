84c79a29091e50883774b3636000f2f312a170d6
malte1989 <echo89@web.de>
2016-12-28 15:41:23 +0100

    Lektion 12: Mit impliziten Intent andere App starten in Android

--- app/src/main/java/de/programmierenlernenhq/malte/programmierenlernen/AktiendetailActivity.java
+++ app/src/main/java/de/programmierenlernenhq/malte/programmierenlernen/AktiendetailActivity.java
@@ -5,8 +5,10 @@ package de.programmierenlernenhq.malte.programmierenlernen;
  */
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -37,7 +39,45 @@ public class AktiendetailActivity extends AppCompatActivity {
             startActivity(new Intent(this, EinstellungenActivity.class));
             return true;
         }
+        if (id == R.id.action_starte_browser) {
+            zeigeWebseiteImBrowser();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void zeigeWebseiteImBrowser(){
+
+        String webseitenURL = "";
+
+        // Die AktiendetailActivity wurde über einen Intent aufgerufen
+        // Wir lesen aus dem empfangenen Intent die übermittelten Daten aus
+        // und bauen daraus die URL der aufzurufenden Webseite
+        Intent empfangenerIntent = this.getIntent();
+        if (empfangenerIntent != null && empfangenerIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            String aktienInfo = empfangenerIntent.getStringExtra(Intent.EXTRA_TEXT);
+
+            int pos = aktienInfo.indexOf(":");
+            String symbol = aktienInfo.substring(0, pos);
+            webseitenURL  = "http://finance.yahoo.com/q?s=" + symbol;
+        }
+
+        // Erzeugen des Data-URI Scheme für die anzuzeigende Webseite
+        // Mehr Infos auf der "Common Intents" Seite des Android Developer Guides:
+        // http://developer.android.com/guide/components/intents-common.html#Browser
+        Uri webseitenUri = Uri.parse(webseitenURL);
+
+        // Erzeugen eines impliziten View-Intents mit der Data URI-Information
+        Intent intent = new Intent(Intent.ACTION_VIEW, webseitenUri);
+
+        // Prüfen ob eine Web-App auf dem Android Gerät installiert ist
+        // und Starten der App mit dem oben erzeugten impliziten View-Intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            String LOG_TAG = AktiendetailActivity.class.getSimpleName();
+            Log.d(LOG_TAG, "Keine Web-App installiert!");
+        }
     }
 }
