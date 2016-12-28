b9b3a5f52e3cb84f67803e09a15168f0a4441aef
malte1989 <echo89@web.de>
2016-12-28 09:42:47 +0100

    Lektion 6: Options Menu und ActionBar

--- app/src/main/java/de/programmierenlernenhq/malte/programmierenlernen/AktienlisteFragment.java
+++ app/src/main/java/de/programmierenlernenhq/malte/programmierenlernen/AktienlisteFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +20,31 @@ import java.util.List;
 public class AktienlisteFragment extends Fragment{
 
     public AktienlisteFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Menü bekannt geben, dadurch kann unser Fragment Menü-Events verarbeiten
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_aktienlistefragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Wir prüfen, ob Menü-Element mit der ID "action_daten_aktualisieren"
+        // ausgewählt wurde und geben eine Meldung aus
+        int id = item.getItemId();
+        if (id == R.id.action_daten_aktualisieren) {
+            Toast.makeText(getActivity(), "Aktualisieren gedrückt!", Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
